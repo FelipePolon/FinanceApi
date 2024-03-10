c1a80f4b20b60d693bdcc5374f768d6485fb4103
Felipe Polon <f.s.polon@gmail.com>
2024-03-10 16:24:53 -0300

    Add the models class and propertys for stocks and comments. Establish the conection between the database and the code by building the "ApplicationDbContext.cs" file and moddifying the "Program.cs" and "appsettings.json" files. And lastly, add the migrations by running the commands "dotnet ef migrations add Init" and "dotnet ef database update".

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
