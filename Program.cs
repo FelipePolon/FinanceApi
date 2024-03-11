30bc5f5ada1865376293f07c9fcfad0375b93f33
Felipe Polon <f.s.polon@gmail.com>
2024-03-10 21:33:27 -0300

    Now in the Comments, build the Http GetAll request with all necessary to make it run properly.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,10 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+
+
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 
 var app = builder.Build();
